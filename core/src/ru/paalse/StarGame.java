3973aaabd0f51e4daab04cda6b461a8a89fce97d
paalse <paalse@list.ru>
2020-02-17 15:04:44 +0400

    Lesson_1

--- core/src/ru/paalse/StarGame.java
+++ core/src/ru/paalse/StarGame.java
@@ -15,19 +15,15 @@ public class StarGame extends ApplicationAdapter {
 	@Override
 	public void create () { // Отвечает за инициализацию
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg"); // Загрузка текстуры из папки assets
-		region = new TextureRegion(img, 20,25,100,50);
+		img = new Texture("star_sky.jpg"); // Загрузка текстуры из папки assets
 	}
 
 	@Override
 	public void render () { // Отвечает за периоодическую отрисовку текстур
-		Gdx.gl.glClearColor(1, 0.5f, 0, 1); // Установка фона
+		Gdx.gl.glClearColor(0, 0, 0, 1); // Установка фона
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);				 	// Очистка экрана
 		batch.begin();
-		batch.setColor(0.1f,0.9f, 0.5f, 0.9f);
 		batch.draw(img,0,0);
-		batch.setColor(0.6f,0.3f, 0.2f, 0.3f);
-		batch.draw(region, 200,200);
 		batch.end();
 	}
 	
